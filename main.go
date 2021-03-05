e9aae596ac419ee2f63e73d00c7d1cf260ecbeb0
Arthur Santana <artsantana1457@gmail.com>
2021-03-05 12:04:31 -0300

    chore: setup env vars on app start

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/Mangaba-Labs/mangaba-stocks.git/database"
 	"github.com/Mangaba-Labs/mangaba-stocks.git/pkg/cli"
+	"github.com/Mangaba-Labs/mangaba-stocks.git/pkg/domain/config"
 )
 
 func main() {
+	config.SetupEnvVars()
 	database.SetupDatabase()
 	cli.StartApp()
 	defer database.Instance.Close()
